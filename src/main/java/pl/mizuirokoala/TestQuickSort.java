a481d5212f04e8e40f118d5f006ed5f7b693884c
Damian <Mizuirokoala@gmail.com>
2018-11-15 13:17:37 +0100

    update test for int and String variable

--- src/main/java/pl/mizuirokoala/TestQuickSort.java
+++ src/main/java/pl/mizuirokoala/TestQuickSort.java
@@ -3,7 +3,8 @@ package pl.mizuirokoala;
 public class TestQuickSort {
 
     public static void main(String[] args) {
-        Integer[] list = {34, 3, 53, 2, 23, 7, 14, 10};
+       // Integer[] list = {34, 3, 53, 2, 23, 7, 14, 10};
+        String[] list = {"a","q","a","da","ca","ca","qwqw","sx","sac","sa","aq","x","v","z","a"};
         Quick qs = new Quick();
         qs.sort(list);
         for (int i = 0; i < list.length; i++) {
