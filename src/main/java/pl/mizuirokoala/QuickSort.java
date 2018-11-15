f2b7c0b00c3e52dcf1f058837efcb6a19fa7a6c4
Damian <Mizuirokoala@gmail.com>
2018-11-15 12:29:29 +0100

    QuickSort vol.1

--- src/main/java/pl/mizuirokoala/QuickSort.java
+++ src/main/java/pl/mizuirokoala/QuickSort.java
@@ -23,45 +23,48 @@ public class QuickSort {
 
     private static int partition(Comparable[] a, int lo, int hi) {
 
-        int i=lo,j=hi+1;
-        Comparable v=a[lo];
-        while (true){
-            while (less(a[++i], v)){
-                if (i==hi){
+        int i = lo, j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+            while (less(a[++i], v)) {
+                if (i == hi) {
                     break;
                 }
             }
-            while (less(v, a[--j])){
-                if (j==lo){
+            while (less(v, a[--j])) {
+                if (j == lo) {
                     break;
                 }
             }
-            if (i>=j){
+            if (i >= j) {
                 break;
             }
-            exch(a,i,j);
+            exch(a, i, j);
         }
-        exch(a,lo,j);
+        exch(a, lo, j);
         return j;
     }
-    private static boolean less(Comparable v, Comparable w){
-        return v.compareTo(w)<0;
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
     }
 
-    private static void exch(Comparable[]a,int i,int j){
-        Comparable com=a[i];
-        a[i]=a[j];
-        a[j]=com;
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable com = a[i];
+        a[i] = a[j];
+        a[j] = com;
     }
-    private static void show(Comparable[] a){
-        for (int i=0;i<a.length;i++){
-            StdOut.print(a[i]+ " ");
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
         }
         StdOut.println();
     }
-    public static boolean isSorted(Comparable[] a){
-        for (int i=0;i<a.length;i++){
-            if (less(a[i], a[i-1])){
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
                 return false;
             }
         }
@@ -69,9 +72,9 @@ public class QuickSort {
     }
 
     public static void main(String[] args) {
-        String[]a=new In().readAllStrings();
+        String[] a = new In().readAllStrings();
         sort(a);
-        assert  isSorted(a);
+        assert isSorted(a);
         show(a);
     }
 }
